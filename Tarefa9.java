ef7d3f39e89bfe9808c76c79f40e293b2adf5ceb
Leonardo Pimenta <117233547+leoopimenta@users.noreply.github.com>
2023-05-24 17:51:26 -0300

    first commit

---
+++ Tarefa9.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Tarefa9 {
+    
+    public static void main(String[] args) {
+            
+    
+    
+        Scanner leitor = new Scanner(System.in);
+    
+        System.out.println("Digite a sua idade: ");
+        
+        int id1 = leitor.nextInt();
+
+        Integer idade = id1;
+    
+        System.out.println("A idade digitada é " + idade);
+    }
+
+}
